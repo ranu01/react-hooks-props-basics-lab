3a5b8d988f6b5b5f52e459d7e7159aa216c107e7
lewis karanu <lewiskaranu0@gmail.com>
2024-04-30 10:43:18 +0300

    adds

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About({bio,github,likedin}) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {!bio || bio ===""?null:<p>{bio}</p>}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       {/* add your <Links /> component here */}
+      <links github={github} likedin={likedin}></links>
     </div>
   );
 }
